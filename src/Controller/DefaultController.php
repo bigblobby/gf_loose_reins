a97e3f1fbca3003a4518a4b656282ebb551cbe90
Tom <tom_dempster@hotmail.co.uk>
2019-03-02 10:45:22 +0000

    Navigation desktop build

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_default_homepage")
+     */
+    public function homepageAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
