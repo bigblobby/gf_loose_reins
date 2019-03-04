9ab63450820e6a96aa8b955da6edc78ed8489a7a
Tom <tom_dempster@hotmail.co.uk>
2019-03-04 21:49:28 +0000

    Create first dynamic page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Navigation;
+use App\Entity\Page;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +19,25 @@ class DefaultController extends AbstractController
 
         return $this->render('default/homepage.html.twig', [
             'links' => $links
+        ]);
+    }
+
+
+    /**
+     * @Route("/{slug}", name="app_default_interim_page")
+     */
+    public function interimPageAction(EntityManagerInterface $em, $slug)
+    {
+        // Links for main navigation
+        $links = $em->getRepository(Navigation::class)->findAll();
+
+        // Get the one main link, then find all of its sub links
+        $mainLink = $em->getRepository(Navigation::class)->findOneBy(['slug' => $slug]);
+        $subLinks = $mainLink->getPages();
+
+        return $this->render('default/interim-page.html.twig', [
+            'links' => $links,
+            'subLinks' => $subLinks
         ]);
     }
 }
