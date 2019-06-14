cdeb4e0fa2f6ef1e9e5aaa7acf9e6e81f2073f2c
Tom <tom_dempster@hotmail.co.uk>
2019-06-14 20:12:23 +0100

    Fixed lazy loading

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,3 +1,52 @@
+// If you are forEach-ing over an array DO NOT splice the same array
+// and expect the looping to continue. It will NOT, it will break the loop.
+// you have to reverse over the array with a for loop so you don't break the indexing.
+// splice will disrupt the indexing.
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+//     let active = false;
+//
+//     function lazyLoad() {
+//         if (active === false) {
+//             active = true;
+//
+//             setTimeout(function() {
+//                 lazyImages.forEach(function(lazyImage, index) {
+//                     console.log(lazyImage, index);
+//                     if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+//                         lazyImage.src = lazyImage.dataset.src;
+//
+//
+//                         lazyImages.splice(index, 1);
+//
+//                         lazyImage.classList.remove("lazy");
+//                         // Remove listeners when all lazy images have loaded
+//                         if (lazyImages.length === 0) {
+//                             document.removeEventListener("scroll", lazyLoad);
+//                             window.removeEventListener("resize", lazyLoad);
+//                             window.removeEventListener("orientationchange", lazyLoad);
+//                         }
+//                     }
+//                 });
+//
+//                 active = false;
+//             }, 200);
+//         }
+//     }
+//
+//     lazyLoad();
+//
+//     document.addEventListener("scroll", lazyLoad);
+//     window.addEventListener("resize", lazyLoad);
+//     window.addEventListener("orientationchange", lazyLoad);
+// });
+
+
+
+/*----------READ ABOVE FOR WHY THIS HAS BEEN KEPT----------*/
+
 document.addEventListener("DOMContentLoaded", function() {
     let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
     let active = false;
@@ -7,18 +56,13 @@ document.addEventListener("DOMContentLoaded", function() {
             active = true;
 
             setTimeout(function() {
-                lazyImages.forEach(function(lazyImage, index) {
-                    if ((lazyImage.getBoundingClientRect().top + 500 <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-                        lazyImage.src = lazyImage.dataset.src;
-                        //lazyImage.srcset = lazyImage.dataset.srcset;
-                        lazyImage.classList.remove("lazy");
+                for(i = lazyImages.length - 1; i >= 0; i -=1){
+                    if ((lazyImages[i].getBoundingClientRect().top <= window.innerHeight && lazyImages[i].getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImages[i]).display !== "none") {
+                        lazyImages[i].src = lazyImages[i].dataset.src;
 
-                        lazyImages.splice(index, 1);
+                        lazyImages.splice(i, 1);
 
-                        // lazyImages = lazyImages.filter(function(image) {
-                        //     return image !== lazyImage;
-                        // });
-
+                        lazyImages[i].classList.remove("lazy");
                         // Remove listeners when all lazy images have loaded
                         if (lazyImages.length === 0) {
                             document.removeEventListener("scroll", lazyLoad);
@@ -26,12 +70,13 @@ document.addEventListener("DOMContentLoaded", function() {
                             window.removeEventListener("orientationchange", lazyLoad);
                         }
                     }
-                });
-
+                }
                 active = false;
-            }, 10);
+            }, 200);
         }
     }
+
+    lazyLoad();
 
     document.addEventListener("scroll", lazyLoad);
     window.addEventListener("resize", lazyLoad);
